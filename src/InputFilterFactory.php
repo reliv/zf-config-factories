bd4d9a2f2e8a022c194d4cd1a475d7cb61e4f201
Rod Mcnew <rmcnew@relivinc.com>
2015-07-27 14:44:57 -0500

    add input filter factory

---
+++ src/InputFilterFactory.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * View Helper Config Driven Abstract Factory
+ *
+ * PHP version 5
+ *
+ * @category  ZF2 Modules
+ * @package   RmFactoriesAsConfiguration
+ * @author    Rod Mcnew
+ * @copyright 2014 Rod Mcnew
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+
+namespace Reliv\FactoriesAsConfiguration;
+
+use Zend\ServiceManager\AbstractFactoryInterface;
+
+/**
+ * View Helper Config Driven Abstract Factory
+ *
+ * PHP version 5
+ *
+ * @category  ZF2 Modules
+ * @package   RmFactoriesAsConfiguration
+ * @author    Rod Mcnew
+ * @copyright 2014 Rod Mcnew
+ * @license   License.txt New BSD License
+ * @version   Release: <package_version>
+ * @link      https://github.com/reliv
+ */
+class InputFilterFactory extends ServiceFactory implements AbstractFactoryInterface
+{
+    /**
+     * @var string the config key of the target service manager
+     */
+    protected $serviceMgrKey = 'input_filters';
+
+    /**
+     * @var bool used know it we must look for the real service locator inside the given service locator
+     */
+    protected $serviceMgrIsRoot = false;
+}
